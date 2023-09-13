6d6c2a95b6284159a5f4fe9684873f4fc196b77c
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-09-13 16:32:51 -0700

    104-fibonacci.c corrected

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main _ The main function
- * 1 and 2, seperated by a comma
- * Return: 0.
+ * main -  a program that finds and prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0 (Sucess)
  */
 
 
