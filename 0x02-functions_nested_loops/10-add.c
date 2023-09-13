5bf454ce0a8bec2dc5a0ed006dc132efb9654f60
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-09-13 13:06:19 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * Description: A program that add two numbers.
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: sum
+ *
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
