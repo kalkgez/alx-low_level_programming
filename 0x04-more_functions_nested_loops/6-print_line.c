565929026e68d1459becc0268616310d84a56439
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-09-15 05:28:54 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - A function that prints n number of lines.
+ * @n: The number of _
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
