9c4a6faa8a9f13fc1a303bad4ab29118efa2f792
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-09-21 09:42:55 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, m;
+
+	m = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (m < size)
+	{
+		j = size - m < 10 ? size - m : 10;
+		printf("%08x: ", m);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + m + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + m + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		m += 10;
+	}
+}
