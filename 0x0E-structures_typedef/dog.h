e7e91f4eda5bf25dc3e1fac695449a5413d48a21
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-10-09 04:47:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+# ifndef DOG_H
+# define DOG_H
+/**
+ * struct dog - A structure that holds an information
+ *
+ * @name: The first member
+ * @age: The second member
+ * @owner: The third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+# endif
