d66673278f9bed54abf07ec3bb533c27e20d0e8e
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-10-19 12:00:59 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - A function that frees a list_t list.
+ *
+ * @head: list to be freed
+ *
+ * Return: void;
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
