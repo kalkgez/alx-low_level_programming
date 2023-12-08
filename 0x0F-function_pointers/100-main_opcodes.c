97751fa179163a15d0dbfb95bbe916a1a2a81488
kalkidan Gezahegn <kalkgezahegn@gmail.com>
2023-12-07 23:22:17 -0800

    moving the tasks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A function that prints its own opcodes
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	int j = 0;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	while (j < bytes)
+	{
+		if (j == bytes - 1)
+		{
+			printf("%02hhx\n", arr[j]);
+			break;
+		}
+		printf("%02hhx ", arr[j]);
+		j++;
+	}
+	return (0);
+}
